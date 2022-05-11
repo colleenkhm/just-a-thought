5a06989c0559c068776c235eecc0ca28805690b9
Colleen Murray <colleenkhm@gmail.com>
2022-05-11 12:43:16 -0500

    pushing to heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,22 +4,22 @@ const router = require("express").Router();
 
 // TODO: create route to get information from page
 router.get("/notes", (req, res) => {
-    const data = fs.readFileSync("./db/db.json");
+    const data = fs.readFileSync("../Develop/db/db.json");
     res.json(JSON.parse(data));
 })
 // TODO: create route to post information to page
 router.post("/notes", (req, res) => {
-    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const notes = JSON.parse(fs.readFileSync("../Develop/db/db.json"));
     const addNote = req.body;
     addNote.id = uuid.v4();
     notes.push(addNote);
-    fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+    fs.writeFileSync("../Develop/db/db.json", JSON.stringify(notes));
     res.json(notes);
 })
 
 // TODO: create route to delete information from page
 router.delete("/notes/:id", (req, res) => {
-    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const notes = JSON.parse(fs.readFileSync("../Develop/db/db.json"));
     const deleteNote = notes.filter(( dltNote ) => dltNote.id !== req.params.id)
 
     res.json(deleteNote);
