ea6b714241c4503d620b810a7e39b4eba3be8cd1
Colleen Murray <colleenkhm@gmail.com>
2022-05-19 13:01:21 -0500

    changed port to PORT for debugging

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const htmlRoutes = require("./routes/htmlRoutes.js");
 const apiRoutes = require("./routes/apiRoutes.js")
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
